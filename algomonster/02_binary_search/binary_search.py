cea1805b042d74573b1d9058b25685881f77f08e
sourav <souravas007@gmail.com>
2026-02-13 14:50:52 +0530

    Add binary search

---
+++ algomonster/02_binary_search/binary_search.py
@@ -0,0 +1,16 @@
+def binary_search(nums, target):
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+
+nums = [1, 3, 5, 6, 7, 8, 10]
+print(binary_search(nums, 5))
+print(binary_search(nums, 4))
