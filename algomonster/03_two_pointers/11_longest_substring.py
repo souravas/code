885c22a3193c9ba5c4740e378b3a3c85f02d0cba
sourav <souravas007@gmail.com>
2026-02-15 22:37:29 +0530

    Add longest substring

---
+++ algomonster/03_two_pointers/11_longest_substring.py
@@ -0,0 +1,13 @@
+def longest_substring_without_repeating_characters(s: str) -> int:
+    seen = set()
+    left = 0
+    longest = 0
+
+    for right in range(len(s)):
+        while s[right] in seen:
+            seen.remove(s[left])
+            left += 1
+        seen.add(s[right])
+        longest = max(longest, right - left + 1)
+
+    return longest
