155aa47dcf258e2588134c2c5bfa9dbc4122d981
sourav <souravas007@gmail.com>
2026-02-21 19:25:24 +0530

    Add subtree of another tree

---
+++ algomonster/04_depth_first_search/07_subtree_of_another_tree.py
@@ -0,0 +1,30 @@
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def subtree_of_another_tree(root: Node, sub_root: Node) -> bool:
+    def check_same(root, sub_root):
+        if not root and not sub_root:
+            return True
+        if not root or not sub_root:
+            return False
+
+        if root.val != sub_root.val:
+            return False
+
+        return check_same(root.left, sub_root.left) and check_same(
+            root.right, sub_root.right
+        )
+
+    if not root:
+        return False
+
+    if check_same(root, sub_root):
+        return True
+
+    return subtree_of_another_tree(root.left, sub_root) or subtree_of_another_tree(
+        root.right, sub_root
+    )
