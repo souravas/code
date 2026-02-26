00257758097f0837e9d83f4d36d66ca4339d62b3
sourav <souravas007@gmail.com>
2026-02-26 21:58:13 +0530

    Add num of ways to decode

---
+++ algomonster/05_backtracking/12_num_ways_to_decode.py
@@ -0,0 +1,23 @@
+def decode_ways(digits: str) -> int:
+    def dfs(index):
+        if index == len(digits):
+            return 1
+        if index in memo:
+            return memo[index]
+
+        if digits[index] == "0":
+            return 0
+
+        ways = 0
+        ways += dfs(index + 1)
+
+        if digits[index] == "1":
+            ways += dfs(index + 2)
+        if digits[index] == "2" and index < (len(digits) - 1):
+            if int(digits[index + 1]) < 7:
+                ways += dfs(index + 2)
+        memo[index] = ways
+        return memo[index]
+
+    memo = {}
+    return dfs(0)
