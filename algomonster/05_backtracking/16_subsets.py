8aed6a45042b27c7d7ef7bb8639486669ca93d9a
sourav <souravas007@gmail.com>
2026-02-28 01:15:51 +0530

    Update subsets

--- algomonster/05_backtracking/16_subsets.py
+++ algomonster/05_backtracking/16_subsets.py
@@ -1,6 +1,6 @@
 def subsets(nums: list[int]) -> list[list[int]]:
     result = []
-    nums.sort()
+    # nums.sort()
 
     def dfs(index, current):
         if index == len(nums):
@@ -11,6 +11,22 @@ def subsets(nums: list[int]) -> list[list[int]]:
         dfs(index + 1, current)
         current.pop()
         dfs(index + 1, current)
+
+    dfs(0, [])
+    return result
+
+
+def subsets_optimal(nums: list[int]) -> list[list[int]]:
+    result = []
+    # nums.sort()
+
+    def dfs(index, current):
+        result.append(current[:])
+
+        for i in range(index, len(nums)):
+            current.append(nums[i])
+            dfs(i + 1, current)
+            current.pop()
 
     dfs(0, [])
     return result
