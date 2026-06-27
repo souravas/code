f8bf437163443556791fc314d40c45846fc0ecd5
sourav <souravas007@gmail.com>
2026-06-28 00:58:12 +0530

    Update balanced binary tree

--- algomonster/04_depth_first_search/06_balanced_binary_tree.py
+++ algomonster/04_depth_first_search/06_balanced_binary_tree.py
@@ -5,7 +5,7 @@ class Node:
         self.right = right
 
 
-def is_balanced(tree: Node) -> bool:
+def is_balanced1(tree: Node) -> bool:
     def dfs(root):
         nonlocal balanced
         if not balanced or not root:
@@ -22,3 +22,15 @@ def is_balanced(tree: Node) -> bool:
     balanced = True
     dfs(tree)
     return balanced
+
+
+def is_balanced2(tree: Node) -> bool:
+    def dfs(root):
+        if not root:
+            return [True, 0]
+        left_balanced, left_val = dfs(root.left)
+        right_balanced, right_val = dfs(root.right)
+        balanced = left_balanced and right_balanced and (abs(abs(left_val) - abs(right_val)) <= 1)
+        return [balanced, 1 + max(left_val, right_val)]
+    is_balanced, _ = dfs(tree)
+    return is_balanced
