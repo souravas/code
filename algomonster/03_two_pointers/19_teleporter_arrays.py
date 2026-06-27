d725ae21074f73265f8f583d0255a29c9eb51e87
sourav <souravas007@gmail.com>
2026-06-27 18:38:46 +0530

    Update teleporter arrays

--- algomonster/03_two_pointers/19_teleporter_arrays.py
+++ algomonster/03_two_pointers/19_teleporter_arrays.py
@@ -1,38 +1,35 @@
-MODULO_AMT = 10**9 + 7
-
-
 def maximum_score(arr1: list[int], arr2: list[int]) -> int:
+    modulo_amount = 10**9 + 7
     result = 0
-    ptr1 = 0
-    ptr2 = 0
+    index1 = 0
+    index2 = 0
+
     n1 = len(arr1)
     n2 = len(arr2)
 
     section_sum1 = 0
     section_sum2 = 0
 
-    while ptr1 < n1 or ptr2 < n2:
-        if ptr1 < n1 and ptr2 < n2 and arr1[ptr1] == arr2[ptr2]:
-            result += max(section_sum1, section_sum2) + arr1[ptr1]
-            result %= MODULO_AMT
+
+    while index1 < n1 and index2 < n2:
+        if arr1[index1] == arr2[index2]:
+            result += max(section_sum1, section_sum2) + arr1[index1]
+            result %= modulo_amount
             section_sum1 = 0
             section_sum2 = 0
-            ptr1 += 1
-            ptr2 += 1
-            continue
-        if ptr1 < n1 and ptr2 < n2:
-            if arr1[ptr1] < arr2[ptr2]:
-                section_sum1 += arr1[ptr1]
-                ptr1 += 1
-            else:
-                section_sum2 += arr2[ptr2]
-                ptr2 += 1
+            index1 += 1
+            index2 += 1
+        elif arr1[index1] < arr2[index2]:
+            section_sum1 += arr1[index1]
+            index1 += 1
         else:
-            if ptr1 == n1:
-                section_sum2 += arr2[ptr2]
-                ptr2 += 1
-            else:
-                section_sum1 += arr1[ptr1]
-                ptr1 += 1
+            section_sum2 += arr2[index2]
+            index2 += 1
+    while index1 < n1:
+        section_sum1 += arr1[index1]
+        index1 += 1
+    while index2 < n2:
+        section_sum2 += arr2[index2]
+        index2 += 1
     result += max(section_sum1, section_sum2)
-    return result % MODULO_AMT
+    return result % modulo_amount
