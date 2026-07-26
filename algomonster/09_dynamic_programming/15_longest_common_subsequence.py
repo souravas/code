5a61217bd48bafcb5b1340949e7c8e91455155c8
sourav <souravas007@gmail.com>
2026-07-26 22:27:36 +0530

    Update longest common subsequence

--- algomonster/09_dynamic_programming/15_longest_common_subsequence.py
+++ algomonster/09_dynamic_programming/15_longest_common_subsequence.py
@@ -13,3 +13,19 @@ def longest_common_subsequence(word1: str, word2: str) -> int:
             return max(dfs(i + 1, j), dfs(i, j + 1))
 
     return dfs(0, 0)
+
+
+from functools import cache
+
+
+def longest_common_subsequence_improved(word1: str, word2: str) -> int:
+
+    @cache
+    def dfs(index1, index2):
+        if index1 < 0 or index2 < 0:
+            return 0
+        if word1[index1] != word2[index2]:
+            return max(dfs(index1 - 1, index2), dfs(index1, index2 - 1))
+        return 1 + (dfs(index1 - 1, index2 - 1))
+
+    return dfs(len(word1) - 1, len(word2) - 1)
