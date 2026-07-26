98e420600309cef97e71d0f6bcbe797df44f96e9
sourav <souravas007@gmail.com>
2026-07-26 15:20:14 +0530

    Update min cost climbing stairs

--- algomonster/09_dynamic_programming/05_min_cost_climbing_stairs.py
+++ algomonster/09_dynamic_programming/05_min_cost_climbing_stairs.py
@@ -9,3 +9,17 @@ def min_cost_climbing_stairs(cost: list[int]) -> int:
 
     cache = {}
     return min(dfs(0), dfs(1))
+
+
+from functools import cache
+
+
+def min_cost_climbing_stairs_improved(cost: list[int]) -> int:
+
+    @cache
+    def helper(index):
+        if index < 0:
+            return 0
+        return min(cost[index] + helper(index - 1), cost[index] + helper(index - 2))
+
+    return min(helper(len(cost) - 1), helper(len(cost) - 2))
