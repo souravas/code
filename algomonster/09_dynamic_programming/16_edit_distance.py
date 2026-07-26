d701a492c57a7d50edddd7a08eb320f39d847e37
sourav <souravas007@gmail.com>
2026-07-26 23:42:33 +0530

    Update edit distance

--- algomonster/09_dynamic_programming/16_edit_distance.py
+++ algomonster/09_dynamic_programming/16_edit_distance.py
@@ -21,3 +21,26 @@ def min_distance(word1: str, word2: str) -> int:
         return min(insert, delete, replace)
 
     return solve(0, 0)
+
+
+def min_distance_improved(word1: str, word2: str) -> int:
+
+    @cache
+    def dfs(index1, index2):
+        if index1 < 0 and index2 < 0:
+            return 0
+        elif index1 < 0:
+            return index2 + 1
+        elif index2 < 0:
+            return index1 + 1
+
+        if word1[index1] == word2[index2]:
+            return dfs(index1 - 1, index2 - 1)
+
+        return 1 + min(
+            dfs(index1 - 1, index2 - 1),
+            dfs(index1, index2 - 1),
+            dfs(index1 - 1, index2),
+        )
+
+    return dfs(len(word1) - 1, len(word2) - 1)
