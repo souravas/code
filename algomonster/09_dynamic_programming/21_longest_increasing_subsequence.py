02bdc20a3001ff23f8a7936a4ab29408fcdadfc0
sourav <souravas007@gmail.com>
2026-07-28 00:44:52 +0530

    Update longest increasing subsequence

--- algomonster/09_dynamic_programming/21_longest_increasing_subsequence.py
+++ algomonster/09_dynamic_programming/21_longest_increasing_subsequence.py
@@ -14,3 +14,24 @@ def longest_sub_len(nums: list[int]) -> int:
     for i in range(len(nums)):
         result = max(result, dfs(i))
     return result
+
+
+from math import inf
+from functools import cache
+
+
+def longest_sub_len_improved(nums: list[int]) -> int:
+
+    @cache
+    def dfs(index, previous):
+        if index == len(nums):
+            return 0
+
+        current = nums[index]
+        take = 0
+        if current > previous:
+            take = 1 + dfs(index + 1, current)
+        skip = dfs(index + 1, previous)
+        return max(take, skip)
+
+    return dfs(0, -inf)
