938ab8f37dda6d227650d6a1b3794d55a0977d7e
sourav <souravas007@gmail.com>
2026-07-27 23:09:02 +0530

    Update distinct subsequences

--- algomonster/09_dynamic_programming/18_distinct_subsequences.py
+++ algomonster/09_dynamic_programming/18_distinct_subsequences.py
@@ -16,3 +16,22 @@ def distinct_subsequences(s: str, t: str) -> int:
         return result
 
     return dfs(0, 0)
+
+
+from functools import cache
+
+
+def distinct_subsequences_improved(s: str, t: str) -> int:
+    @cache
+    def dfs(index1, index2):
+        if index2 < 0:
+            return 1
+        if index1 < 0:
+            return 0
+        result = 0
+        if s[index1] == t[index2]:
+            result += dfs(index1 - 1, index2 - 1)
+        result += dfs(index1 - 1, index2)
+        return result
+
+    return dfs(len(s) - 1, len(t) - 1)
