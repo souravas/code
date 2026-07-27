e828757543baf1bd0daff75a1d1a4b93f6fcb049
sourav <souravas007@gmail.com>
2026-07-28 00:48:58 +0530

    Update partition array for maximum sum

--- algomonster/09_dynamic_programming/22_partition_array_for_maximum_sum.py
+++ algomonster/09_dynamic_programming/22_partition_array_for_maximum_sum.py
@@ -21,25 +21,3 @@ def partition_array_for_maximum_sum(arr: list[int], k: int) -> int:
         return max_sum
 
     return dfs(0)
-
-
-from functools import cache
-
-
-def partition_array_for_maximum_sum_alternative(arr: list[int], k: int) -> int:
-
-    @cache
-    def dfs(index):
-        if index >= len(arr):
-            return 0
-
-        max_sum = 0
-        for i in range(index + 1, min(index + k + 1, len(arr) + 1)):
-            current_max = 0
-            for j in range(index, i):
-                current_max = max(current_max, arr[j])
-            current_result = (current_max * (i - index)) + dfs(i)
-            max_sum = max(max_sum, current_result)
-        return max_sum
-
-    return dfs(0)
