2840d009a20c5e409dd296e7ec61cc25b0462918
sourav <souravas007@gmail.com>
2026-08-02 17:17:02 +0530

    Update lru cache

--- algomonster/10_advanced_data_structures/13_lru_cache.py
+++ algomonster/10_advanced_data_structures/13_lru_cache.py
@@ -1,7 +1,50 @@
 from collections import OrderedDict
 
 
+class Node:
+    def __init__(self, key: int, val: int) -> None:
+        self.key, self.val = key, val
+        self.prev = self.next = None
+
+
 class LRUCache:
+    def __init__(self, capacity: int) -> None:
+        self.capacity = capacity
+        self.cache: dict[int, Node] = {}
+        # head.next is the most recently used, tail.prev the least
+        self.head, self.tail = Node(0, 0), Node(0, 0)
+        self.head.next, self.tail.prev = self.tail, self.head
+
+    def _remove(self, node: Node) -> None:
+        node.prev.next, node.next.prev = node.next, node.prev
+
+    def _insert(self, node: Node) -> None:
+        node.prev, node.next = self.head, self.head.next
+        node.next.prev = node
+        self.head.next = node
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        node = self.cache[key]
+        self._remove(node)
+        self._insert(node)
+        return node.val
+
+    def put(self, key: int, value: int) -> None:
+        if self.capacity <= 0:
+            return
+        if key in self.cache:
+            self._remove(self.cache[key])
+        self.cache[key] = node = Node(key, value)
+        self._insert(node)
+        if len(self.cache) > self.capacity:
+            lru = self.tail.prev
+            self._remove(lru)
+            del self.cache[lru.key]
+
+
+class LRUCacheImproved:
     def __init__(self, capacity: int) -> None:
         self.capacity = capacity
         self.cache: OrderedDict[int, int] = OrderedDict()
