ab38765af7db56ff762cead307922d0dd0644018
sourav <souravas007@gmail.com>
2026-08-02 19:42:16 +0530

    Add prime sieve

---
+++ algomonster/11_miscellaneous/24_prime_sieve.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+def prime_sieve(n: int) -> List[bool]:
+    primes = [True] * (n + 1)
+    primes[0] = primes[1] = False
+    for i in range(2, n + 1, 1):
+        if primes[i]:
+            for j in range(i + i, n + 1, i):
+                primes[j] = False
+    return primes
