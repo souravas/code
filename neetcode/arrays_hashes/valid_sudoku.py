83cbe5bf106f4993f588bf84b309b4260d302b6d
sourav <souravas007@gmail.com>
2026-08-02 17:17:43 +0530

    Update valid sudoku

--- neetcode/arrays_hashes/valid_sudoku.py
+++ neetcode/arrays_hashes/valid_sudoku.py
@@ -2,7 +2,38 @@ from collections import defaultdict
 
 
 class Solution:
-    def isValidSudoku(self, board: list[list[str]]) -> bool:
+    def isValidSudoku1(self, board: list[list[str]]) -> bool:
+        def valid_rows():
+            for row in board:
+                seen = set()
+                for element in row:
+                    if element in seen and element != ".":
+                        return False
+                    seen.add(element)
+            return True
+
+        def valid_columns():
+            for i in range(len(board)):
+                seen = set()
+                for j in range(len(board[0])):
+                    if board[j][i] in seen and board[j][i] != ".":
+                        return False
+                    seen.add(board[j][i])
+            return True
+
+        def valid_grids():
+            seen = defaultdict(set)
+            for i in range(len(board)):
+                for j in range(len(board[0])):
+                    current = board[i][j]
+                    if current in seen[(i // 3, j // 3)] and current != ".":
+                        return False
+                    seen[(i // 3, j // 3)].add(current)
+            return True
+
+        return valid_rows() and valid_columns() and valid_grids()
+
+    def isValidSudoku2(self, board: list[list[str]]) -> bool:
         row = defaultdict(set)
         col = defaultdict(set)
         grid = defaultdict(set)
